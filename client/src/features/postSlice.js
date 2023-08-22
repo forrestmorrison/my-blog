f68f1cb06efb3f0c1f114428c81f95103ae9254f
forrestmorrison <forrestmorrison@gmail.com>
2023-08-22 15:55:27 -0500

    changed body to content in CreatePost & postSlice

--- client/src/features/postSlice.js
+++ client/src/features/postSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit"
 export const postSlice = createSlice({
     name: "post",
     initialState: {
-        value: { title: "", body: "" }
+        value: { title: "", content: "" }
     },
     reducers: {
         submitPost: (state, action) => {
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -5,7 +5,7 @@ import { Button, TextField } from "@mui/material"
 
 const CreatePost = (props) => {
   const [title, setTitle] = useState("")
-  const [body, setBody] = useState("")
+  const [content, setContent] = useState("")
 
   const dispatch = useDispatch()
 
@@ -13,8 +13,8 @@ const CreatePost = (props) => {
     setTitle(e.target.value)
   }
 
-  const handleBodyChange = (e) => {
-    setBody(e.target.value)
+  const handleContentChange = (e) => {
+    setContent(e.target.value)
   }
 
   return (
@@ -28,14 +28,14 @@ const CreatePost = (props) => {
           onChange={handleTitleChange}
           required
         />
-        <h4>body</h4>
+        <h4>content</h4>
         <TextField 
           sx={{
             width: "100%"
           }}
           multiline
           rows={12}
-          onChange={handleBodyChange}
+          onChange={handleContentChange}
           required
         />
         <Button
@@ -50,7 +50,7 @@ const CreatePost = (props) => {
           }}
           type="submit"
           onClick={() => {
-            dispatch(submitPost({ title: title, body: body }))
+            dispatch(submitPost({ title: title, content: content }))
           }}
         >
           Submit
