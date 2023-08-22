57c2a79a6bf0fb3c3bfad7b78a5d53f306c73b8d
forrestmorrison <forrestmorrison@gmail.com>
2023-08-22 15:01:28 -0500

    added favicon, created server index.js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const port = process.env.PORT || 4000
+const bodyParser = require("body-parser")
+const app = express()
+const cors = require("cors")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}`)
+})
