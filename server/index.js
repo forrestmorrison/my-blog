23464e98d8528585cd0560d0f7923df51a3f12d9
forrestmorrison <forrestmorrison@gmail.com>
2023-08-28 08:40:23 -0500

    added SQL connection files

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const bodyParser = require("body-parser")
 const app = express()
 const cors = require("cors")
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+
 
 app.use(cors())
 
