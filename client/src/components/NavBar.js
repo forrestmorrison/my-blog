cb09e7282ae1dfb81ffde497348c7d5ee7d148b3
forrestmorrison <forrestmorrison@gmail.com>
2023-08-29 21:36:29 -0500

    added ternary operator code to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,37 +2,75 @@ import { Link } from 'react-router-dom';
 import Logo from "../images/my-blog-logo.png"
 
 const NavBar = () => {
-  return (
-    <div className="navbar">
-        <Link to="/"
-            style={{
-                height: "100%",
-                display: "flex",
-                alignItems: "center"
-            }}
-        >
-            <img src={Logo} alt="logo" className="logo" />
-        </Link>
-        <div className="links">
-            <Link to="/createpost"
-                style={{
-                    margin: "0 10px",
-                    fontWeight: "bold",
-                }}
-            >
-                <a href='/createpost'>new post</a>
-            </Link>
-            <Link to="/"
-                style={{
-                    margin: "0 10px",
-                    fontWeight: "bold",
-                }}
-            >
-                <a href='/'>log out</a>
-            </Link>
+
+    const user = true
+
+    return (
+        <div className="container">
+            { user ? (
+                <div className="navbar">
+                    <Link to="/"
+                        style={{
+                            height: "100%",
+                            display: "flex",
+                            alignItems: "center"
+                        }}
+                    >
+                        <img src={Logo} alt="logo" className="logo" />
+                    </Link>
+                    <div className="links">
+                        <Link to="/createpost"
+                            style={{
+                                margin: "0 10px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            <a href='/createpost'>new post</a>
+                        </Link>
+                        <Link to="/"
+                            style={{
+                                margin: "0 10px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            <a href='/'>log out</a>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div className="navbar">
+                    <Link to="/"
+                        style={{
+                            height: "100%",
+                            display: "flex",
+                            alignItems: "center"
+                        }}
+                    >
+                        <img src={Logo} alt="logo" className="logo" />
+                    </Link>
+                    <div className="links">
+                        <Link to="/createpost"
+                            style={{
+                                margin: "0 10px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            <a href='/'>log in</a>
+                        </Link>
+                        <Link to="/"
+                            style={{
+                                margin: "0 10px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            <a href='/'>sign up</a>
+                        </Link>
+                    </div>
+                </div>
+                )   
+            }        
         </div>
-    </div>
-  )
+    )
 }
 
 export default NavBar
