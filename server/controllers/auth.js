c815d0af6e3ec350cba16ce9c5c2f4cb10f59d72
forrestmorrison <forrestmorrison@gmail.com>
2023-09-06 21:22:23 -0500

    added JWT & BCrypt code to auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -10,11 +10,45 @@ const signUp = (req, res) => {
     const { name, email, password } = req.body
     let sql = "INSERT INTO USERS (name, email, password) VALUES (?, ?, ?)"
 
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        sql = mysql.format(sql, [name, email, hash])
+
+        pool.query(sql, (err, result) => {
+            console.log("result", result)
+            if (err) {
+                if (err.code === "ER_DUP_ENTRY") return res.status(409).send("email is already registered")
+                return handleSQLError(res, err)
+            }
+            return logInUser(email, password, res)
+        })
+    })
+
 }
 
 const logInUser = (email, password, res) => {
     let sql = "SELECT * FROM users WHERE email = ?"
     sql = mysql.format(sql, [ email ])
+
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        if (!rows.length) return res.status(404).send("No matching users")
+
+        const hash = rows[0].password
+        bcrypt.compare(password, hash)
+            .then(result => {
+                if (!result) return res.status(400).send("Invalid password")
+
+                const data = { ...rows[0]}
+                data.password = "REDACTED"
+
+                const token = jwt.sign(data, process.env.JWT_SECRIET)
+                res.json({
+                    msg: "Log in successful",
+                    token,
+                    user: data
+                })
+            })
+    })
 }
 
 const logIn = (req, res) => {
@@ -26,8 +60,13 @@ const logOut = (req, res) => {
     res.json({})
 }
 
+const checkUser = (req, res) => {
+    res.send(req.user)
+}
+
 module.exports = {
     signUp,
     logIn,
-    logOut
+    logOut,
+    checkUser
 }
