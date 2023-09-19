cadc9b3f4e57caa17d56d5d74f9c074ade1f40fa
forrestmorrison <forrestmorrison@gmail.com>
2023-09-18 20:36:39 -0500

    added Auth0 code

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const express = require("express")
+const { auth } = require("express-openid-connect")
 const port = process.env.PORT || 4000
 const bodyParser = require("body-parser")
 const app = express()
 const cors = require("cors")
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_URL
+  };
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-
 app.use(cors())
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 app.listen(port, () => {
     console.log(`Web server is listening on port ${port}`)
