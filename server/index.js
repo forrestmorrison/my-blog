c31b64d05157b51a7bd9c9fe204c8f4739c43403
forrestmorrison <forrestmorrison@gmail.com>
2023-09-22 05:34:27 -0500

    added News API get request

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { auth } = require("express-openid-connect")
+const request = require("request")
 const port = process.env.PORT || 4000
 const bodyParser = require("body-parser")
 const app = express()
@@ -12,7 +13,7 @@ const config = {
     baseURL: process.env.AUTH0_BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
     issuerBaseURL: process.env.AUTH0_ISSUER_URL
-  };
+};
 
 // // auth router attaches /login, /logout, and /callback routes to the baseURL
 // app.use(auth(config));
@@ -27,7 +28,18 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-app.get(`https://newsapi.org/v2/everything?q=Apple&from=2023-09-19&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`)
+app.get(`/everything?q=Apple&from=2023-09-19&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`, (req, res) => {
+    request(
+        { url: `https://newsapi.org/v2/everything?q=Apple&from=2023-09-19&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`},
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: "error", message: error.message })
+            }
+
+            res.json(JSON.parse(body))
+        }
+    )
+})
 
 app.listen(port, () => {
     console.log(`Web server is listening on port ${port}`)
